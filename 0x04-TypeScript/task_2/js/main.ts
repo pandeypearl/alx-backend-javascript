f8584d4a5e4d56f107ce73a31cc67959182929c7
Pretty Pandey <prettypandeypearl@gmail.com>
2023-07-05 02:56:29 -0700

    Task 7 : String literal types

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -62,3 +62,15 @@ export function executeWork(employee: (Director | Teacher)) {
   }
   return (employee as Teacher).workTeacherTasks();
 }
+
+// Subjects
+export type Subjects = ('Math' | 'History');
+
+export function teachClass(todayClass: Subjects): string {
+  if (todayClass === 'Math') {
+    return 'Teaching Math';
+  }
+  if (todayClass === 'History') {
+    return 'Teaching History';
+  }
+}

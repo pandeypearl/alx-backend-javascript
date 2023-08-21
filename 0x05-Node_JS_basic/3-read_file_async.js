1241e48ebe1874417270c36128d4af5c321c898a
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-21 10:27:16 +0000

    Task 3. Reading a file asynchronously with Node JS

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,36 @@
+/**
+ * Reads read the database file synchronously
+ */
+const fs = require('fs');
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path,'utf8', (error, stRecords) => {
+      if (error) reject(new Error('Cannot load the database'));
+      else {
+        const content = stRecords.split('\n');
+  	const cs = []
+  	const swe = []
+
+        content.forEach((stRecord) => {
+    	  const field = stRecord.split(',');
+
+    	  if (field !== [] && field !== null) {
+     	    if (field[3] == 'CS') {
+              cs.push(field[0]);
+      	    } else if (field[3] == 'SWE') {
+              swe.push(field[0]);
+            }
+          }
+        });
+	
+	console.log(`Number of students: ${cs.length + swe.length}`);
+  	console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  	console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+ 	resolve();
+      }
+    });
+  });
+}
+
+module.exports = countStudents;

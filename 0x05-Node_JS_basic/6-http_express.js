b71db43cda4d74a18d6b0846bc22220a1f846ae2
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-21 14:58:18 +0000

    Task 6. Create a small HTTP server using Express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,15 @@
+/**
+ * Small HTTP server using express
+ */
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
+
+module.exports = app;

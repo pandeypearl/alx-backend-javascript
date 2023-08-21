8f53b75e37582ff03c6d480f04d33f65f04dfed9
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-21 09:59:59 +0000

    Task 2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,36 @@
+/**
+ * Reads read the database file synchronously
+ */
+const fs = require('fs');
+
+function countStudents(filepath) {
+  let content;
+
+  try {
+    content = fs.readFileSync(filepath, { encoding: 'utf8', flag: 'r' });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+
+  stRecords = content.split('\n');
+  const cs = []
+  const swe = []
+
+  stRecords.forEach((stRecord) => {
+    const field = stRecord.split(',');
+    
+    if (field !== [] && field !== null) {
+      if (field[3] == 'CS') {
+        cs.push(field[0]);
+      } else if (field[3] == 'SWE') {
+        swe.push(field[0]);
+      }
+    }
+  });
+
+  console.log(`Number of students: ${cs.length + swe.length}`);
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+}
+
+module.exports = countStudents;

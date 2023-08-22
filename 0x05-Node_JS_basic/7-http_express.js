5847e74b9119f438cb0ec4ecd2814e28530cbd6a
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-22 21:06:23 +0200

    Task 7 code fix

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -9,7 +9,7 @@ const port = 1245;
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
-    fs.readfile(path, 'utf8', (err, records) => {
+    fs.readFile(path, 'utf8', (err, records) => {
       if (err) reject(new Error('Cannot load the database'));
       else {
         const content = records.split('\n');

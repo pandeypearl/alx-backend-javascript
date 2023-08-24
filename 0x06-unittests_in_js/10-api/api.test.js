a199b42ca2483a261e30690a524a649ca83b8e78
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-24 17:59:15 +0200

    TAsk 10 code fix

--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -21,15 +21,32 @@ describe('API integration test', () => {
     });
 
     it('GET /cart/:id returns 404 response for valid negative number values in :id', (done) => {
-        request.get(`${API_URL}/cart/-37`, (_err, res, body) => {
+        request.get(`${API_URL}/cart/-37`, (_err, res, _body) => {
             expect(res.statusCode).to.be.equal(404);
             done();
         });
     });
 
     it('GET /cart/:id returns 404 response for valid non-numeric values in :id', (done) => {
-        request.get(`${API_URL}/cart/d400-55b5-5gu7`, (_err, res, body) => {
+        request.get(`${API_URL}/cart/d400-55b5-5gu7`, (_err, res, _body) => {
             expect(res.statusCode).to.be.equal(404);
+            done();
+        });
+    });
+
+    it('POST /login returns valid response', (done) => {
+        request.get(`${API_URL}/login`, {json: {userName: 'Pretty'}}, (_err, res, body) => {
+            expect(res.statusCode).to.be.equal(200);
+            expect(body).to.be.equal('Welcome Pretty');
+            done();
+        });
+    });
+
+    it('GET /available_payments returns valid response', (done) => {
+        request.get(`${API_URL}/available_payments`, (_err, res, body) => {
+            expect(res.statusCode).to.be.equal(200);
+            expect(JSON.parse(body))
+                .to.be.deep.equal({payment_methods: {credit_cards: true, paypal: false}})
             done();
         });
     });

b3ae30cf8580f2d024cc548786f3444e07154ace
Pretty Pandey <prettypandeypearl@gmail.com>
2023-08-24 13:19:15 +0200

    Task 6. Async tests with done

---
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -0,0 +1,11 @@
+/**
+ * Payment Token
+ * @param { bool } success
+ */
+const getPaymentTokenFromAPI = (success) => new Promise((resolve, _reject) => {
+    if (success) {
+        resolve({data: 'Successful response from the API'});
+    }
+});
+
+module.exports = getPaymentTokenFromAPI;
